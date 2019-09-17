202d1f8024571f85eb1ebc8fda14e0003167f977
martin donadieu <martindonadieu@gmail.com>
2019-09-17 11:57:55 +0200

    make keeping system better

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -4,8 +4,10 @@ uri="--uri $MONGO_URL"
 options="${MONGODUMP_OPTIONS:- }"
 timestamp=`date +%Y-%m-%d_%Hh%M`
 authSource=${MONGO_AUTHSOURCE:-admin}
-keep_day=${KEEP_DAY:-10}
+keep_backup=${KEEP_BACKUP:-12}
+mode="${1:-daily}"
 
+echo "Mode: $mode"
 if [ -z "$MONGO_URL" ]; then
   echo "Required environment variable MONGO_URL not found"
   exit 42
@@ -19,7 +21,7 @@ for database in ${DATABASES[@]}
 do
     echo "---------------------------------"
     echo "Start to dump" ${database}
-    output_file="$database-$timestamp.gz"
+    output_file="$mode-$database-$timestamp.gz"
     output_command="/dump/$output_file"
     if mongodump $uri$database?authSource=$authSource $options --gzip --archive="$output_command" ; then
       echo "Dump $database succeeded"
@@ -33,7 +35,8 @@ do
     echo "---------------------------------"
 done
 
-# remove dumps older than 10 days
-find /dump/ -type f -name '*.dump' -mtime +$keep_day -exec rm {} \;
+# remove dumps older than 10(keep_backup) files
+rm -rf $(ls -1t . | tail -n +$keep_backup | grep $mode)
+# find /dump/ -type f -name '$mode-*.gz' -mtime +$keep_backup -exec rm {} \;
 
 
